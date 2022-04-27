57ff6ecede651fea0523ec7fe0911aeaee684786
Brad Larson <bhlarson@gmail.com>
2022-04-27 09:44:14 -0700

    Added documentaiton and test for data loaders

--- tests/test_cocostore.py
+++ tests/test_cocostore.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader
 from pymlutil.s3 import s3store, Connect
 from pymlutil.jsonutil import ReadDict
 from pymlutil.imutil import ImUtil, ImTransform
-from torchdatasetutil.cocostore import CocoStore
+from torchdatasetutil.cocostore import CocoStore, CreateCocoLoaders
 
 test_config = 'test.yaml'
 class Test(unittest.TestCase):      
@@ -28,7 +28,7 @@ class Test(unittest.TestCase):
 
         s3, creds, s3def = Connect(parameters['coco']['credentials'])
 
-        dataset_desc = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['coco']['dataset_train'])
+        dataset_desc = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['coco']['dataset_val'])
         class_dictionary = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['coco']['class_dict']) 
         imUtil = ImUtil(dataset_desc, class_dictionary)
                 
@@ -39,15 +39,59 @@ class Test(unittest.TestCase):
 
         for i, iman in enumerate(store):
             img = store.MergeIman(iman['img'], iman['ann'])
+            is_success, buffer = cv2.imencode(".png", img)
+            if not is_success:
+                raise ValueError('test_cocostore test_iterator cv2.imencode failure  image {}'.format(i))
             if img is None:
                 raise ValueError('img is None')
             if 'test_images' in parameters['coco'] and i >= parameters['coco']['test_images']:
                 break
 
+    '''
+    default_loaders = [{'set':'train', 'dataset': 'data/coco/annotations/instances_train2017.json', 'image_path':'data/coco/train2017' , 'enable_transform':True},
+                    {'set':'test', 'dataset': 'data/coco/annotations/instances_val2017.json', 'image_path':'data/coco/val2017', 'enable_transform':False}]
+
+    def CreateCocoLoaders(s3, bucket, class_dict, 
+                        batch_size = 2, shuffle=True, 
+                        num_workers=0, cuda = True, timeout=0, loaders = default_loaders, 
+                        height=640, width=640, 
+                        image_transform=None, label_transform=None, 
+                        normalize=True, flipX=True, flipY=False, 
+                        rotate=3, scale_min=0.75, scale_max=1.25, offset=0.1, astype='float32',
+                        random_seed = None):
+    '''
 
 
-    def test_dataset(self):
-        print('test_dataset: create me!')
+
+    def test_CreateCocoLoaders(self):
+        parameters = ReadDict(test_config)
+
+        if 'coco' not in parameters:
+            raise ValueError('coco not in {}'.format(test_config))
+
+        s3, creds, s3def = Connect(parameters['coco']['credentials'])
+
+        dataset_desc = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['coco']['dataset_val'])
+        class_dictionary = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['coco']['class_dict']) 
+        imUtil = ImUtil(dataset_desc, class_dictionary)
+
+        loaders = CreateCocoLoaders(s3, bucket=s3def['sets']['dataset']['bucket'],
+                                     class_dict=parameters['coco']['class_dict'])
+
+        for i, data in enumerate(loaders[0]['dataloader']):
+            inputs, labels, mean, stdev = data
+            images = inputs.cpu().permute(0, 2, 3, 1).numpy()
+            labels = np.around(labels.cpu().numpy()).astype('uint8')
+            mean = mean.cpu().numpy()
+            stdev = stdev.cpu().numpy()
+
+            for j, image in enumerate(images):
+                img = imUtil.MergeIman(images[j], labels[j], mean[j], stdev[j])
+                is_success, buffer = cv2.imencode(".png", img)
+                if not is_success:
+                    raise ValueError('test_imstore test_CreateCocoLoaders cv2.imencode failure batch {} image {}'.format(i, j))
+            if 'test_images' in parameters['coco'] and i >= parameters['coco']['test_images']:
+                break
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_imstore.py
+++ tests/test_imstore.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader
 from pymlutil.s3 import s3store, Connect
 from pymlutil.jsonutil import ReadDict
 from pymlutil.imutil import ImUtil, ImTransform
-from torchdatasetutil.imstore import ImagesStore
+from torchdatasetutil.imstore import ImagesStore, CreateImageLoaders
 
 test_config = 'test.yaml'
 class Test(unittest.TestCase):      
@@ -34,13 +34,44 @@ class Test(unittest.TestCase):
 
         for i, iman in enumerate(store):
             img = store.MergeIman(iman['img'], iman['ann'])
+            is_success, buffer = cv2.imencode(".png", img)
+            if not is_success:
+                raise ValueError('test_imstore test_iterator cv2.imencode failure  image {}'.format(i))
             if img is None:
                 raise ValueError('img is None')
             if 'test_images' in parameters['images'] and i >= parameters['images']['test_images']:
                 break
     
-    def test_dataset(self):
-        print('test_dataset: create me!')
+    def test_CreateImageLoaders(self):
+        parameters = ReadDict(test_config)
+
+        if 'images' not in parameters:
+            raise ValueError('images not in {}'.format(test_config))
+
+        s3, creds, s3def = Connect(parameters['images']['credentials'])
+
+        dataset_desc = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['images']['dataset'])
+        class_dictionary = s3.GetDict(s3def['sets']['dataset']['bucket'],parameters['images']['class_dict']) 
+        imUtil = ImUtil(dataset_desc, class_dictionary)
+
+        loaders = CreateImageLoaders(s3, bucket=s3def['sets']['dataset']['bucket'],
+                                     dataset_dfn=parameters['images']['dataset'],
+                                     class_dict=parameters['images']['class_dict'])
+
+        for i, data in enumerate(loaders[0]['dataloader']):
+            inputs, labels, mean, stdev = data
+            images = inputs.cpu().permute(0, 2, 3, 1).numpy()
+            labels = np.around(labels.cpu().numpy()).astype('uint8')
+            mean = mean.cpu().numpy()
+            stdev = stdev.cpu().numpy()
+
+            for j, image in enumerate(images):
+                img = imUtil.MergeIman(images[j], labels[j], mean[j], stdev[j])
+                is_success, buffer = cv2.imencode(".png", img)
+                if not is_success:
+                    raise ValueError('test_imstore test_CreateImageLoaders cv2.imencode failure batch {} image {}'.format(i, j))
+            if 'test_images' in parameters['images'] and i >= parameters['images']['test_images']:
+                break
 
 if __name__ == '__main__':
     unittest.main()
