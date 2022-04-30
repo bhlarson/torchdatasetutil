7bceb95f14840dd9d87ee89d089a172b14188f4a
Brad Larson <bhlarson@gmail.com>
2022-04-29 17:01:01 -0700

    Add gitcoco diagnostics

--- torchdatasetutil/getcoco.py
+++ torchdatasetutil/getcoco.py
@@ -6,7 +6,7 @@ import subprocess
 import shutil
 from os import fspath
 import tempfile
-from pymlutil.s3 import s3store
+from pymlutil.s3 import s3store, Connect
 from zipfile import ZipFile
 from tqdm import tqdm
 
@@ -23,10 +23,9 @@ cocourl=["http://images.cocodataset.org/zips/train2017.zip",
         ]
 
 def getcoco(s3, s3def, cocourl=cocourl, dataset='coco'):
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-
-        for url in cocourl:
+    saved_name = '{}/{}'.format(s3def['sets']['dataset']['prefix'] , dataset)
+    for url in tqdm(cocourl):
+        with tempfile.TemporaryDirectory() as tmpdir:
             outpath = '{}/{}'.format(tmpdir,os.path.basename(url))
             if os.path.isfile(outpath):
                 print('{} exists.  Skipping'.format(outpath))
@@ -40,12 +39,53 @@ def getcoco(s3, s3def, cocourl=cocourl, dataset='coco'):
                 for file in tqdm(iterable=zip_ref.namelist(), total=len(zip_ref.namelist())):
                     zip_ref.extract(member=file, path=fspath(dest))
 
-            os.remove(outpath) # Remove zip file once extracted
+            os.remove(outpath) # Remove zip
 
-        saved_name = '{}/{}'.format(s3def['sets']['dataset']['prefix'] , dataset)
-        print('Save {} to {}/{}'.format(tmpdir, s3def['sets']['dataset']['bucket'], saved_name))
-        if s3.PutDir(s3def['sets']['dataset']['bucket'], tmpdir, saved_name):
-            shutil.rmtree(tmpdir, ignore_errors=True)
+            print('Save {} to {}/{}'.format(dest, s3def['sets']['dataset']['bucket'], saved_name))
+            if s3.PutDir(s3def['sets']['dataset']['bucket'], dest, saved_name):
+                shutil.rmtree(tmpdir, ignore_errors=True)
 
-        url = s3.GetUrl(s3def['sets']['dataset']['bucket'], saved_name)
-        print("Complete. Results saved to {}".format(url))
+    url = s3.GetUrl(s3def['sets']['dataset']['bucket'], saved_name)
+    print("Complete. Results saved to {}".format(url))
+
+def Test(args):
+
+    s3, _, s3def = Connect(args.credentails)
+
+    if args.min:
+        args.cocourl = [args.cocourl[-2], args.cocourl[-1]]
+
+    getcoco(s3, s3def, cocourl=args.cocourl, dataset='coco_test')
+
+    print('{} {} complete'.format(__file__, __name__))
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process arguments')
+
+    parser.add_argument('-d', '--debug', action='store_true',help='Wait for debuggee attach')   
+    parser.add_argument('-debug_port', type=int, default=3000, help='Debug port')
+    parser.add_argument('-debug_address', type=str, default='0.0.0.0', help='Debug port')
+    parser.add_argument('-dataset_path', type=str, default='./dataset', help='Local dataset path')
+    parser.add_argument('-credentails', type=str, default='creds.yaml', help='Credentials file.')
+    parser.add_argument('-min', action='store_true',help='Minimum test')
+    parser.add_argument('-cocourl', type=json.loads, default=cocourl, 
+                        help='List of coco dataset URLs to load.  If none, coco 2017 datafiles are loaded from https://cocodataset.org/#download')
+
+
+    args = parser.parse_args()
+    return args
+    
+if __name__ == '__main__':
+    import argparse
+    args = parse_arguments()
+
+    if args.debug:
+        print("Wait for debugger attach on {}:{}".format(args.debug_address, args.debug_port))
+        import debugpy
+
+        debugpy.listen(address=(args.debug_address, args.debug_port)) # Pause the program until a remote debugger is attached
+        debugpy.wait_for_client()  # Pause the program until a remote debugger is attached
+        print("Debugger attached")
+
+    Test(args)
