8245f4bbd699869678838e772a70e3f478e2d07b
Brad Larson <bhlarson@gmail.com>
2022-09-16 11:56:29 -0700

    adding cifar10store and supporing s3 download of object storage

--- torchdatasetutil/cifar10store.py
+++ torchdatasetutil/cifar10store.py
@@ -10,14 +10,13 @@ from collections import defaultdict
 import torch
 from tqdm import tqdm
 from torch.utils.data import Dataset
+import torchvision
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 from pymlutil.s3 import s3store, Connect
 from pymlutil.jsonutil import ReadDict
 from pymlutil.imutil import ImUtil, ImTransform
-
-
 
 class AddGaussianNoise(object):
     def __init__(self, mean=0., std=1.):
@@ -31,83 +30,57 @@ class AddGaussianNoise(object):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
 
-def CreateImagesetLoaders(s3, s3def, src, dest, bucket = None, width=256, height=256, batch_size = 2, shuffle=True, 
-                      num_workers=0, cuda = True, timeout=0, loaders = None, 
-                      image_transform=None, label_transform=None, 
-                      normalize=True, flipX=True, flipY=False, 
-                      random_seed = None, numTries=3):
+default_loaders = [{'set':'train', 'enable_transform':True},
+                   {'set':'test', 'enable_transform':False}]
 
-    if not bucket:
-        bucket = s3def['sets']['dataset']['bucket']
+def CreateCifar10Loaders(dataset_path, batch_size = 2,  
+                      num_workers=0, cuda = True, loaders = default_loaders, 
+                      rotate=3, scale_min=0.75, scale_max=1.25, offset=0.1):
 
-    if not os.path.exists(dest):
-        s3.GetDir(bucket, src, dest)
-
-    dest = os.path.join(dest, '') #Ensure there is a trailing slash
+    Cifar10Classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     pin_memory = False
     if cuda:
         pin_memory = True
 
-
-    # Load dataset
-    if loaders is None:
-        train_transform = transforms.Compose([
-            transforms.RandomAffine(degrees=10, 
-               translate=(0.1, 0.1), 
-               scale=(0.9, 1.1), 
-               interpolation=transforms.InterpolationMode.BILINEAR),
-            transforms.RandomCrop( (width, height), padding=None, pad_if_needed=True, fill=0, padding_mode='constant'),
-            transforms.ToTensor(), 
-            transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)), # Imagenet mean and standard deviation
-            transforms.RandomHorizontalFlip(p=0.5),
-            #AddGaussianNoise(0., 0.05)
-        ])
-
-        test_transform = transforms.Compose([
-            transforms.RandomCrop( (width, height), padding=None, pad_if_needed=True, fill=0, padding_mode='constant'),
-            transforms.ToTensor(), 
-            transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)) # Imagenet mean and standard deviation
-        ])
-
-        default_loaders = [{'set':'train', 'dataset': dest, 'enable_transform':True, 'transform':train_transform},
-                        {'set':'val', 'dataset': dest, 'enable_transform':False, 'transform':train_transform}]
-
-        loaders = default_loaders
-
-    startIndex = 0
-    allocated = 0.0
-
     for i, loader in enumerate(loaders):
+        if loader['enable_transform']:
+            transform = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomAffine(degrees=rotate,
+                    translate=(offset, offset), 
+                    scale=(scale_min, scale_max), 
+                    interpolation=transforms.InterpolationMode.BILINEAR),
+                transforms.ToTensor(),
+                transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)), # Imagenet mean and standard deviation
+                AddGaussianNoise(0., args.augment_noise)
+            ])
+        else:
+            transform = transforms.Compose([transforms.ToTensor(), 
+                    transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)) # Imagenet mean and standard deviation
+                ])
 
-
-
-        imagenet_data = datasets.ImageNet(loader['dataset'], split=loader['set'], transform=loader['transform'])
-        loader['dataloader'] = torch.utils.data.DataLoader(imagenet_data,
-                                                batch_size=batch_size,
-                                                shuffle=shuffle,
-                                                num_workers=num_workers,
-                                                pin_memory=pin_memory)
-
+        dataset = torchvision.datasets.CIFAR10(root=dataset_path, train=loader['set']=='train', download=True, transform=transform)
         # Creating PT data samplers and loaders:
-        loader['batches'] =int(imagenet_data.__len__()/batch_size)
-        loader['length'] = loader['batches']*batch_size      
-
+        loader['width']=32
+        loader['height']=32
+        loader['in_channels']=3
+        loader['num_classes']=len(Cifar10Classes)
+        loader['classes']=list(Cifar10Classes)
+        loader['batches'] =int(len(dataset)/batch_size)
+        loader['length'] = loader['batches']*batch_size
+        loader['dataloader'] = torch.utils.data.DataLoader(dataset=dataset, 
+                                                    batch_size=batch_size,
+                                                    num_workers=num_workers, 
+                                                    pin_memory=pin_memory)
     return loaders
 
 def main(args):
 
-    s3, creds, s3def = Connect(args.credentails)
-
     if args.test_dataset:
-        loaders = CreateImagesetLoaders(s3, s3def, 
-                                    args.obj_src, 
-                                    args.destination, 
-                                    width=args.width, 
-                                    height=args.height, 
-                                    batch_size=args.batch_size, 
-                                    num_workers=args.num_workers,
-                                    cuda = args.cuda)
+        loaders = CreateCifar10Loaders(args.dataset_path, batch_size = args.batch_size,  
+                        num_workers=args.num_workers, cuda = args.cuda, 
+                        rotate=args.augment_rotation, scale_min=args.augment_scale_min, scale_max=args.augment_scale_max, offset=args.augment_translate_x)
+
 
         for loader in tqdm(loaders, desc="Loader"):
             for i, data in tqdm(enumerate(loader['dataloader']), 
@@ -116,8 +89,8 @@ def main(args):
                 sample, target = data
                 assert(sample is not None)
                 assert(sample.shape[0] == args.batch_size)
-                assert(sample.shape[2] == args.height)
-                assert(sample.shape[3] == args.width)
+                assert(sample.shape[2] == loader['height'])
+                assert(sample.shape[3] == loader['height'])
                 assert(target is not None)
 
             if args.num_images is not None and args.num_images > 0 and i >= args.num_images:
@@ -136,6 +109,7 @@ def parse_arguments():
     parser.add_argument('-debug_port', type=int, default=3000, help='Debug port')
     parser.add_argument('-debug_address', type=str, default='0.0.0.0', help='Debug port')
     parser.add_argument('-credentails', type=str, default='creds.yaml', help='Credentials file.')
+    parser.add_argument('-dataset_path', type=str, default='./dataset', help='Local dataset path')
     parser.add_argument('-num_images', type=int, default=0, help='Number of images to display')
     parser.add_argument('-num_workers', type=int, default=25, help='Data loader workers')
     parser.add_argument('-batch_size', type=int, default=4, help='Dataset batch size')
@@ -146,15 +120,15 @@ def parse_arguments():
     parser.add_argument('-test_path', type=str, default='./datasets_test/', help='Test path ending in a forward slash')
     parser.add_argument('-test_config', type=str, default='test.yaml', help='Test configuration file')
 
-    parser.add_argument('-obj_src', type=str, default='data/imagenet', help='Object storage dataset source')
-    #parser.add_argument('-destination', type=str, default='/data/datsets', help='Object storage dataset source')
-    parser.add_argument('-destination', type=str, default='/nvmestore/mlstore/mllib/data/imagenet', help='Object storage dataset source')
-
-    parser.add_argument('-height', type=int, default=256, help='Batch image height')
-    parser.add_argument('-width', type=int, default=256, help='Batch image width')
-    parser.add_argument('-imflags', type=int, default=cv2.IMREAD_COLOR, help='cv2.imdecode flags')
     parser.add_argument('-cuda', type=bool, default=True, help='pytorch CUDA flag') 
     parser.add_argument('-numTries', type=int, default=3, help="Read retries")
+
+    parser.add_argument('-augment_rotation', type=float, default=0.0, help='Input augmentation rotation degrees')
+    parser.add_argument('-augment_scale_min', type=float, default=1.00, help='Input augmentation scale')
+    parser.add_argument('-augment_scale_max', type=float, default=1.00, help='Input augmentation scale')
+    parser.add_argument('-augment_translate_x', type=float, default=0.125, help='Input augmentation translation')
+    parser.add_argument('-augment_translate_y', type=float, default=0.125, help='Input augmentation translation')
+    parser.add_argument('-augment_noise', type=float, default=0.1, help='Augment image noise')
 
     args = parser.parse_args()
 
